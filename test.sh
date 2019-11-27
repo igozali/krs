32977097ee616fed838755aac78315cc07376a7a
Ivan Gozali <gozaliivan@gmail.com>
2019-11-27 15:29:25 -0800

    Easier to test with this method
    Signed-off-by: Ivan Gozali <gozaliivan@gmail.com>

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# See https://stackoverflow.com/questions/59077877 regarding running
+# a teardown function after `cargo run` finishes.
+
+docker-compose down || :
+docker-compose up -d
+
+while ! docker-compose exec kafka kafka-topics --zookeeper zookeeper:2181 --list; do
+    echo "Waiting for Kafka broker to be ready"
+done
+
+cargo test $@
+docker-compose down
--- tests/args.rs
+++ tests/args.rs
@@ -1,11 +1,8 @@
 #[cfg(test)]
-mod utils;
-use utils::{setup, teardown};
-
-use scopeguard::defer;
+use chrono::Utc;
 
 use krs::{make_parser, Config, Sourced};
-use krs::commands::topics::ListCommand;
+use krs::commands::topics::{ListCommand, CreateCommand, DescribeCommand};
 
 macro_rules! assert_ok {
     ($x:expr) => {
@@ -38,11 +35,8 @@ fn test_global_args() {
 }
 
 #[test]
-fn test_list_topics() {
-    let setup = setup();
-    defer! { teardown() };
-
-
+fn test_list_topics_runs() {
+    // Assumes that a Kafka broker is running at localhost:9092
     let cmd = ListCommand::from(Config {
         brokers: Sourced {
             source: "test".to_owned(),
@@ -51,17 +45,50 @@ fn test_list_topics() {
         ..Default::default()
     });
 
-    cmd.run().unwrap();
-
-    println!("{:?}", setup);
-    // assert!(false);
+    assert_ok!(cmd.run());
 }
 
 #[test]
-fn test_run_twice() {
-    let _ = setup();
-    defer! { teardown() };
+fn test_create_and_describe_topics() {
+    // Assumes that a Kafka broker is running at localhost:9092
+    let cmd = CreateCommand::from(Config {
+        brokers: Sourced {
+            source: "test".to_owned(),
+            value: Some("localhost:9092".to_owned())
+        },
+        ..Default::default()
+    });
+
+    let topic_name = format!("krs-topic-{}", Utc::now().timestamp_millis());
+
+    assert_ok!(cmd.run(&topic_name, 1, 1));
+
+    let cmd = DescribeCommand::from(Config {
+        brokers: Sourced {
+            source: "test".to_owned(),
+            value: Some("localhost:9092".to_owned())
+        },
+        zookeeper: Sourced {
+            source: "test".to_owned(),
+            value: Some("localhost:2181".to_owned())
+        },
+        ..Default::default()
+    });
+    assert_ok!(cmd.run(&topic_name));
 }
+
+// #[test]
+// fn test_run_twice() {
+//     let cmd = ListCommand::from(Config {
+//         brokers: Sourced {
+//             source: "test".to_owned(),
+//             value: Some("localhost:9092".to_owned())
+//         },
+//         ..Default::default()
+//     });
+
+//     cmd.run().unwrap();
+// }
 // test that running krs prints usage
 // test that running `krs topics` prints `Incomplete subcommand` error
 // test that running `krs topics invalid-subcommand` prints `Invalid subcommand` error
